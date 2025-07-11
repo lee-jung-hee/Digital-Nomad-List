da22243c717a9176e8cc06943eac722bf5719b91
Thomas Lee <160320465+lee-jung-hee@users.noreply.github.com>
2025-07-11 09:55:28 +0900

    Delete src/createClient.js
    Change DB

--- src/createClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const apiUrl = "https://iqqxymuhmairdikzpske.supabase.co";
-const apiKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxcXh5bXVobWFpcmRpa3pwc2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MzYyNTksImV4cCI6MjA2NzUxMjI1OX0.U2TIGk02vHk3hSi-cS45r3mDxLlMvq27S6r1mA4HEwg";
-
-export const supabase = createClient(apiUrl, apiKey);
